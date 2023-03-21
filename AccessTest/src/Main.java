c23586dae47f1da22a6c0af271bd82d601b7906c
AlimChowdhury <md.alim.chowdhury8@gmail.com>
2023-03-21 20:30:57 +0600

    modify

--- AccessTest/src/Main.java
+++ AccessTest/src/Main.java
@@ -3,6 +3,7 @@ class Test
     int a;
     public int b;
     private int c;
+    protected int d;
 
     void setc(int i)
     {
@@ -13,6 +14,12 @@ class Test
     {
         return c;
     }
+
+}
+
+class Test2
+{
+
 }
 public class Main {
     public static void main(String[] args)
@@ -22,9 +29,10 @@ public class Main {
         ob.a=10;
         ob.b=15;
         ///ob.setc()=100 (error!!! as it is private value)
-
         ob.setc(25);
+        ob.d=20;
+        System.out.println("a, b, c, d :"+ob.a+" "+ob.b +" "+ob.getc()+" "+ob.d);
 
-        System.out.println("a, b, c :"+ob.a+" "+ob.b +" "+ob.getc());
+        Test2 ob2 = new Test2();
     }
 }
